e20442f48f1641290da09205b243bdbfb7290dde
LuizFelipeVPCrema <luizfelipecrema@hotmail.com>
2024-08-16 16:23:16 -0400

    Tela inicial estática, ainda adicionar o Js e Suporte tamanho de telas

--- src/app/page.js
+++ src/app/page.js
@@ -2,112 +2,197 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.js</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+    <main className="flex min-h-screen flex-col justify-between text-black">
+
+      <header className="z-50 fixed bg-yellow-400  w-full items-center justify-evenly font-mono text-sm lg:flex">
+        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t  from-yellow-400 via-yellow-400 lg:static lg:h-auto lg:w-auto lg:bg-none">
           <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+            className="pointer-events-auto flex place-items-center gap-2 p-8"
+            href="https//agrom2.com.br"
             target="_blank"
             rel="noopener noreferrer"
           >
-            By{" "}
+            {/* By{" "} */}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
+              src="/agrom2.svg"
+              alt="Agrom2 Logo"
+              className="dark"
               width={100}
               height={24}
               priority
             />
           </a>
         </div>
+        <div>
+          <a className="pointer-events-auto fixed left-0 top-0 flex w-full justify-center pb-8 pt-8 font-bold text-xl backdrop-blur-2xl  lg:static lg:w-auto">
+            Soluções&nbsp;
+            <Image
+              src="/arrowDown.svg"
+              alt="Arrow Down" 
+              className="dark" 
+              width={15}
+              height={20}
+            />
+          </a>
+        </div>
+        <p className="pointer-events-auto fixed left-0 top-0 flex w-full justify-center pb-8 pt-8 font-bold text-xl backdrop-blur-2xl  lg:static lg:w-auto">
+          Preços e taxas&nbsp;
+          <Image
+            src="/arrowDown.svg"
+            alt="Arrow Down" 
+            className="dark" 
+            width={15}
+            height={20}
+          />
+        </p>
+        <p className="pointer-events-auto fixed left-0 top-0 flex w-full justify-center pb-8 pt-8  font-bold text-xl backdrop-blur-2xl  lg:static lg:w-auto">
+          Sobre a Agro m²&nbsp;
+          <Image
+            src="/arrowDown.svg"
+            alt="Arrow Down" 
+            className="dark" 
+            width={15}
+            height={20}
+          />
+        </p>
+        <p className="pointer-events-auto fixed left-0 top-0 flex w-full justify-center pb-8 pt-8 pr-8 font-bold text-xl backdrop-blur-2xl  lg:static lg:w-auto">
+          Blog&nbsp;
+          <Image
+            src="/arrowDown.svg"
+            alt="Arrow Down" 
+            className="dark" 
+            width={15}
+            height={20}
+          />
+        </p>
+      </header>  
+
+      <div>
+        <div className="h-[1920px] absolute w-full min-h-screen -z-10">
+          <Image
+            className="relative"
+            src="/img/SoybeanPlantation.jpeg"
+            alt="Background SoybeanPlantation"
+            fill
+            priority
+          />
+          <div className="absolute inset-0 bg-yellow-500 opacity-40"></div>
+        </div>
+        <aside className="pl-10 grid grid-cols-4 grid-rows-4 gap-0">
+          <h1 className="font-bold text-6xl col-start-1 col-end-3 row-start-2 row-end-2">
+            Do hectare ao<br/> 
+            metro quadrado
+          </h1>
+          <p className="text-2xl pb-24 col-start-1 col-end-3 row-start-3 row-end-3">
+            Tomada de decisão rápida e integrada é o<br/>
+            que confere competitividade a agricultura<br/> 
+            brasileira.
+          </p>
+        </aside>
+        <div className="h-[920px] grid grid-cols-5 grid-rows-6 gap-0 bg-white">
+          <aside className="col-start-1 col-end-3 row-start-2 row-end-4 ">
+            <ul className="list-disc pl-20  w-6/12 text-5xl font-bold">
+              <li>Análise</li>
+              <li>Fornecimento</li>
+              <li>Entrega</li>
+            </ul>
+            <p className="pl-10 text-2xl">Saiba mais sobre nossos serviços</p>
+          </aside>
+          <div className="col-start-2 col-end-3 row-start-4 row-end-6">
+            <Image 
+              src="/img/FolhaFitoxidade.png"
+              alt="Folha que apresenta fitoxidade"
+              width={200}
+              height={300}
+              />
+              <div className="z-10 border-black border-2 w-24 h-24 transform -translate-x-12 -translate-y-12" /> 
+          </div>
+          <div className="pt-20 col-start-3 col-end-4 row-start-1 row-end-3">
+            <Image 
+              src="/img/FolhaFerrugemDoColmo.png"
+              alt="Folha que apresenta Ferrugem do colmo"
+              width={200}
+              height={300}
+            />
+            <div className="z-10 border-black border-2 w-24 h-24 transform -translate-x-12 -translate-y-12" /> 
+          </div>
+          <div className="col-start-4 col-end-6 row-start-2 row-end-6 -translate-x-12">
+            <Image 
+              className="border-8 border-black rounded-lg"
+              src="/img/FazendeiroTirandoFoto.jpg"
+              alt="Fazendeiro tirando foto da soja"
+              width={700}
+              height={700}
+            />
+          </div>
+        </div>
       </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-full sm:before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full sm:after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50 text-balance`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+      <footer className="bg-[#132116] h-[640px] grid grid-col-5 grid-row-5">
+        <div className="col-start-1 col-end-2 row-start-1 row-end-3 flex justify-center items-center">
+          <a
+              className="pointer-events-auto"
+              href="https//agrom2.com.br"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/agrom2.svg"
+                alt="Agrom2 Logo"
+                className="filter invert"
+                width={150}
+                height={24}
+                priority
+              />
+            </a>
+        </div>
+        <div className="col-start-1 col-end-2 row-start-3 row-end-6 flex justify-center items-center flex-col">
+            <h1 className="font-bold text-3xl -ml-16 text-[#ECB417]">  
+              Quem Somos
+            </h1>
+            <p className="w-64 text-white">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc vulputate libero et velit interdum, ac aliquet odio mattis. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.
+            </p>
+        </div>
+        <div className="col-start-3 col-end-4 row-start-3 row-end-4 flex justify-center ">
+          <div className="flex-col flex space-y-5">
+            <h1 className="font-bold text-3xl text-[#ECB417]">  
+              Suporte
+            </h1>
+            <a className="w-64 text-white font-bold">
+              FAQ's
+            </a>
+            <a className="font-bold text-white">
+              Política de Privacidade
+            </a>
+            <a className="font-bold text-white">
+              Termo & Condições
+            </a>
+          </div>
+        </div>
+        <div className="col-start-4 col-end-5 row-start-1 row-end-6 flex justify-center flex-col space-y-5 ">
+          <h1 className="font-bold text-3xl text-[#ECB417]">  
+            Contato
+          </h1>
+          <div className="flex">
+            <p className="pr-1 text-white">Telefone: </p>
+            <p className="text-white">+55 65 9272-9267</p>
+          </div>
+          <div className="flex">
+            <p className="pr-1 text-white">Email: </p>
+            <p className="text-white">ighor@agrom2.com.br</p>
+          </div>
+          <div className="flex">
+            <p className="pr-1 text-white">Endereço: </p>
+            <p className="max-w-[317px] max-h-[100px] overflow-hidden text-white">Edifício FAMATO, R. Eng. Edgar Prado Arze, S/N - Centro Político Administrativo, Cuiabá - MT, 78050-970</p>
+          </div>
+          <div className="flex">
+            <p className="pr-1  text-white">CNPJ: </p>
+            <p className="text-white">46.082.930/0001-32</p>
+          </div>      
+        </div>
+      </footer>
+  
     </main>
   );
 }
