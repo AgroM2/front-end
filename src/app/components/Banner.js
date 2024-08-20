7f546cde7279b3a057fd65d35f66b2f8e36efc38
LuizFelipeVPCrema <luizfelipecrema@hotmail.com>
2024-08-20 12:57:28 -0400

    Aplicação da Responsividade

--- src/app/components/Banner.js
+++ src/app/components/Banner.js
@@ -1,29 +1,47 @@
 import Image from "next/image";
 
+// const Banner = () => {
+//     return (
+//       <div>
+//         <div className="h-[1920px] absolute w-full -z-10 sm:w-[320px] ">
+//           <Image
+//             className="relative"
+//             src="/imgs/SoybeanPlantation.jpg"
+//             alt="Background SoybeanPlantation"
+//             fill
+//             priority
+//           />
+//           <div className="absolute inset-0 bg-[#ECB417] opacity-40"></div>
+//         </div>
+//         <aside className="pl-10 grid grid-cols-4 grid-rows-4 gap-0">
+//           <h1 className="font-bold text-6xl col-start-1 col-end-3 row-start-2 row-end-2 ">
+//             Do Hectare ao<br/> 
+//             Metro Quadrado
+//           </h1>
+//           <p className="text-2xl pb-24 col-start-1 col-end-3 row-start-3 row-end-3">
+//             Inteligência Artificial e Ciência de Dados<br/> para produção inteligente de commodities.
+//           </p>
+//         </aside>
+//       </div>
+//     );
+// }
+
+// export default Banner;
+
 const Banner = () => {
-    return (
-      <div>
-        <div className="h-[1920px] absolute w-full min-h-screen -z-10">
-          <Image
-            className="relative"
-            src="/imgs/SoybeanPlantation.jpg"
-            alt="Background SoybeanPlantation"
-            fill
-            priority
-          />
-          <div className="absolute inset-0 bg-[#ECB417] opacity-40"></div>
-        </div>
-        <aside className="pl-10 grid grid-cols-4 grid-rows-4 gap-0">
-          <h1 className="font-bold text-6xl col-start-1 col-end-3 row-start-2 row-end-2">
-            Do Hectare ao<br/> 
-            Metro Quadrado
-          </h1>
-          <p className="text-2xl pb-24 col-start-1 col-end-3 row-start-3 row-end-3">
-            Inteligência Artificial e Ciência de Dados<br/> para produção inteligente de commodities.
-          </p>
-        </aside>
+  return (
+    <div className="min-w-max h-screen flex items-center justify-center bg-cover bg-center bg-no-repeat" style={{ backgroundImage: "url('/imgs/SoybeanPlantation.jpg')" }}>
+      <div className="w-full h-full bg-[#ECB417] bg-opacity-40 flex flex-col items-start justify-center p-10">
+        <h1 className="font-bold text-4xl sm:text-5xl md:text-6xl lg:text-7xl leading-tight">
+          Do Hectare ao<br /> 
+          Metro Quadrado
+        </h1>
+        <p className="text-lg sm:text-xl md:text-2xl lg:text-3xl mt-4">
+          Inteligência Artificial e Ciência de Dados<br /> para produção inteligente de commodities.
+        </p>
       </div>
-    );
+    </div>
+  );
 }
 
 export default Banner;
--- src/app/components/Footer.js
+++ src/app/components/Footer.js
@@ -4,41 +4,50 @@ import ContactInfo from "./ContractInfo";
 
 const Footer = () => {
     return (
-        <footer id={"footer"} className="bg-[#132116] h-[640px] grid grid-col-5 grid-row-5">
-        <div className="col-start-1 col-end-2 row-start-1 row-end-3 flex justify-center items-center">
-          <a
-              className="pointer-events-auto"
-              href="#home"
-            >
-              <Image
-                src="/icons/agrom2.svg"
-                alt="Agrom2 Logo"
-                className="filter invert"
-                width={150}
-                height={24}
-                priority
-              />
-            </a>
-        </div>
-        <div className="col-start-1 col-end-2 row-start-3 row-end-6 flex  items-center flex-col">
-            <h1 className="font-bold text-3xl  pb-4 text-[#ECB417]">  
-              Quem Somos
-            </h1>
-            <p className="min-w-64 max-w-96 ml-48 text-white text-justify">
-            A Agro M2 é uma startup que utiliza tecnologia de ponta para transformar o setor agrícola. Com visão computacional, identificamos sintomas foliares na soja e otimizamos a aplicação de produtos químicos através de dados precisos e análises avançadas. Nosso foco em ciência de dados e inteligência artificial aumenta a eficiência e a produtividade das lavouras, garantindo rastreabilidade total desde a semente até a comercialização. Oferecemos transparência e controle em todas as etapas da produção. Participe como usuário beta do nosso software e aplicativo e ajude a moldar o futuro da agricultura. 
-            <a 
-              className="italic underline" 
-              href="https://form.respondi.app/ciDdC3ou" 
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Experimente Primeiro.
-            </a>
-            </p>
-        </div>
-        <SupportInfo />
-        <ContactInfo />
-      </footer>
+        <footer id={"footer"} className="bg-[#132116] h-auto md:w-full pt-10">
+          <div className="grid grid-cols-1 md:grid-row-4 md:grid-cols-2 lg:grid-cols-3">
+            
+            <div className="flex justify-center items-center md:col-start-1 md:row-start-1">
+                <a
+                    className="pointer-events-auto"
+                    href="#home"
+                  >
+                    <Image
+                      src="/icons/agrom2.svg"
+                      alt="Agrom2 Logo"
+                      className="filter invert"
+                      width={150}
+                      height={24}
+                      priority
+                    />
+                  </a>
+            </div>
+            
+            <div className="md:col-start-1 md:row-start-2">
+              <h1 className="font-bold text-3xl text-[#ECB417] pb-4 pt-4 flex justify-center">  
+                Quem Somos
+              </h1>
+              <p className=" pr-8 pl-8 text-white text-justify">
+              A Agro M2 é uma startup que utiliza tecnologia de ponta para transformar o setor agrícola. Com visão computacional, identificamos sintomas foliares na soja e otimizamos a aplicação de produtos químicos através de dados precisos e análises avançadas. Nosso foco em ciência de dados e inteligência artificial aumenta a eficiência e a produtividade das lavouras, garantindo rastreabilidade total desde a semente até a comercialização. Oferecemos transparência e controle em todas as etapas da produção. Participe como usuário beta do nosso software e aplicativo e ajude a moldar o futuro da agricultura. 
+                <a 
+                  className="italic underline" 
+                  href="https://form.respondi.app/ciDdC3ou" 
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  Experimente Primeiro.
+                </a>
+              </p>
+            </div>
+            <div className="flex justify-center pt-4 lg:row-start-1 lg:row-end-2 lg:col-start-2">
+              <SupportInfo />
+            </div>
+            <div className="flex justify-center ml-20 pt-4 lg:row-start-1 lg:row-end-3">
+              <ContactInfo />
+            </div>
+          </div>
+
+        </footer>
     );
 }
 
